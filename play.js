a6bfd1cba405b21abea9a3eba8797ad0976855f4
Luis.BolanosS Luis.BolanosS@perficient.com <Luis.BolanosS@perficient.com>
2021-08-16 14:49:05 -0500

    create pacman game with Juan Pablo

--- play.js
+++ play.js
@@ -1,8 +1,8 @@
 var scenario = document.createElement("canvas");
-var contexto = scenario.getContext("2d");
+var context = scenario.getContext("2d");
 
-scenario.height = 800;
 scenario.width = 1024;
+scenario.height = 800;
 document.body.appendChild(scenario);
 
 var mainImage = new Image();
@@ -13,37 +13,90 @@ mainImage.src = "pac.png";
 var score = 0;
 var enemyScore = 0;
 var initial = true;
+var maxTries = 3;
 
-var pacman = {
-  x: 50,
-  y: 100,
-  mouth: 320,
-  dir: 0,
-  speed: 5
+var playerOne = {
+  x: 64,
+  y: 64,
+  character: 320,
+  face: 0,
+  speed: 5,
+  width: 32,
+  height: 32
 };
 
-var enemy = {
-    x: 150,
-    y: 200,
+var enemyTemplate = {
+    x: 0,
+    y: 0,
+    character: 0,
+    ghost: 0,
     speed: 5,
     moving: 0,
-    mouth: 0,
-    dirx: 0,
-    diry: 0
-}
+    face: 0,
+    dirX: 0,
+    dirY: 0,
+    width: 32,
+    height: 32,
+    initial: true,
+    defeated: false
+};
+
+var faces = {
+    right: 0,
+    down: 32,
+    left: 64,
+    up: 96
+};
+
+var characters = {
+    closedMouth: 320,
+    openedMouth: 352,
+    red: 0,
+    orange: 64,
+    pink: 128,
+    green: 192,
+    purple: 256,
+    blue: 384
+};
+
+var speds = {
+    normal: 5,
+    fast: 10
+};
+
+
+var red = Object.assign({}, enemyTemplate, {character: characters.red, ghost: characters.red, x: 0});
+var orange = Object.assign({}, enemyTemplate, {character: characters.orange, ghost: characters.orange, x: 32});
+var pink = Object.assign({}, enemyTemplate, {character: characters.pink, ghost: characters.pink, x: 64});
+var green = Object.assign({}, enemyTemplate, {character: characters.green, ghost: characters.green, x: 96});
+var purple = Object.assign({}, enemyTemplate, {character: characters.purple, ghost: characters.purple, x: 128});
+
+var enemies = [red, orange, pink, green, purple];
 
 var powerDot = {
     x: 10,
     y: 10,
-    powerUp: false
+    powerUp: false,
+    radio: 5,
+    countDown: 0,
+    ghostNum: 0
 };
+
+var dimensions = {
+    width: scenario.width - (playerOne.width * 2),
+    height: scenario.height - (playerOne.height * 2)
+};
+
 
 var keyclick = {};
 
-    
 function startGame() {
     this.ready = true;
     playgame();
+}
+
+function resetGame() {
+    location.reload();
 }
 
 function playgame() {
@@ -56,19 +109,14 @@ function random(factor) {
 }
 
 function setBackground() {   
-    contexto.fillStyle = "black";
-    contexto.fillRect(0,0,scenario.width, scenario.height);
-    contexto.font = "20px Verdana";
-    contexto.fillStyle = "white";
-    contexto.fillText("Pacman: " + score + " vs Ghoshs " + enemyScore, 2, 18);    
+    context.fillStyle = "black";
+    context.fillRect(0, 0, scenario.width, scenario.height);
+    context.font = "20px Verdana";
+    context.fillStyle = "white";
+    context.fillText("Pacman: " + score + " vs Ghoshs " + enemyScore, 2, 18);    
 }
 
-function render() {
-    setBackground();
-    moveEnemy();
-    defineLimits(enemy);
-
-    
+function drawCharacter(character) {
     // 1: Imagen
     // 2: La figura en x (empieza en cero, multiplicar x 32)
     // 3: La figura en y (empieza en cero, multiplicar x 32)
@@ -78,98 +126,221 @@ function render() {
     // 7: Posicion en y
     // 8: Ancho en x
     // 9: Alto Posicion
-    contexto.drawImage(mainImage, pacman.mouth, pacman.dir, 32, 32, pacman.x, pacman.y, 50, 50);
-    contexto.drawImage(mainImage, enemy.mouth, 0, 32, 32, enemy.x, enemy.y, 32, 32);
+    context.drawImage(mainImage, character.character, character.face, character.width, character.height, character.x, character.y, character.width, character.height);
 }
 
-function movePacman(keyclick) {
+function render() {
+    setBackground();
+    
+    enemies.forEach(enemy => {
+        drawCharacter(enemy);
+        moveEnemy(enemy);
+        checkEnemyCollision(enemy);
+    });
+    
+    drawCharacter(playerOne);
+    movePowerDot();
+    checkTimeoutPowerDot();
+    
+}
+
+function movePlayerOne(keyclick) {
     if (37 in keyclick) {
-        pacman.x -= pacman.speed;
-        pacman.dir = 64
+        playerOne.x -= playerOne.speed;
+        playerOne.face = faces.left;
     }
     
     if (38 in keyclick) {
-        pacman.y -= pacman.speed;
-        pacman.dir = 96
+        playerOne.y -= playerOne.speed;
+        playerOne.face = faces.up;
     }
     
     if (39 in keyclick) {
-        pacman.x += pacman.speed;
-        pacman.dir = 0
+        playerOne.x += playerOne.speed;
+        playerOne.face = faces.right;
     }
     
     if (40 in keyclick) {
-        pacman.y += pacman.speed;
-        pacman.dir = 32
+        playerOne.y += playerOne.speed;
+        playerOne.face = faces.down;
     }
-
-    defineLimits(pacman);
     
-    if (pacman.mouth == 320) {
-        pacman.mouth = 352;
+    if (playerOne.character == characters.closedMouth) {
+        playerOne.character = characters.openedMouth;
     } else {
-        pacman.mouth = 320;
-
+        playerOne.character = characters.closedMouth;
     }
     
+    defineLimits(playerOne);
     render();
 }
 
-function moveEnemy() {
-    if (initial) {
-        enemy.mouth = random(5) * 64;
-        enemy.x = random(956);
-        enemy.y = random(736);
-        initial = false;
+
+function moveEnemy(enemy) {
+    if (enemy.initial) {
+        enemy.x = random(dimensions.width);
+        enemy.y = random(dimensions.height);
+        enemy.initial = false;
     }
 
     if (enemy.moving < 0) {
         enemy.moving = (random(30) * 3 ) + 10 + random(1);
-        enemy.speed = random(4) + 1;
-        enemy.dirx = 0;
-        enemy.diry = 0;
+        enemy.speed = random(5);
+        enemy.dirX = 0;
+        enemy.dirY = 0;
         
         if (enemy.moving % 2) {
-            if (pacman.x < enemy.x) {
-                enemy.dirx = -enemy.speed;
-            } else {
-                enemy.dirx = enemy.speed;
-            }
+            moveEnemyInX(enemy);
         } else{
-            if (pacman.y < enemy.y){
-                enemy.diry = -enemy.speed;
-            } else {
-                enemy.diry = enemy.speed;
-            }
+            moveEnemyInY(enemy);
         }
     }
     
     enemy.moving--;
-    enemy.x = enemy.x + enemy.dirx;
-    enemy.y = enemy.y + enemy.diry;
+    enemy.x = enemy.x + enemy.dirX;
+    enemy.y = enemy.y + enemy.dirY;
+
+    defineLimits(enemy);
 }
 
-function defineLimits(player) {
-    if (player.x >= (scenario.width - 32)) {
-        player.x = 0;
+function defineLimits(character) {
+    if (character.x >= (scenario.width - character.width)) {
+        character.x = 0;
     }
     
-    if (player.x < 0) {
-        player.x = (scenario.width - 32);
+    if (character.x < 0) {
+        character.x = scenario.width - character.width;
     }
     
-    if (player.y >= (scenario.height - 32)) {
-        player.y = 0;
+    if (character.y >= (scenario.height - character.height)) {
+        character.y = 0;
     }
     
-    if (player.y < 0) {
-        player.y = (scenario.height - 32);
+    if (character.y < 0) {
+        character.y = scenario.height - character.height;
+    }
+}
+
+
+function moveEnemyInX(enemy) {
+    if (powerDot.countDown === 0 && playerOne.x < enemy.x) {
+        enemy.dirX = -enemy.speed;
+        changeGhost(enemy, faces.left);
+    } else {
+        enemy.dirX = enemy.speed;
+        changeGhost(enemy, faces.right);
+    }
+}
+
+function moveEnemyInY(enemy) {
+    if (powerDot.countDown === 0 && playerOne.y < enemy.y) {
+        enemy.dirY = - enemy.speed;
+        changeGhost(enemy, faces.up);
+    } else {
+        enemy.dirY = enemy.speed;
+        changeGhost(enemy, faces.down);
+    }
+}
+
+function changeGhost(enemy, face) {
+    if (powerDot.countDown === 0) {
+        enemy.face = face;
+        enemy.character = enemy.ghost;
+        enemy.defeated = false;
+    } else {
+        enemy.character = characters.blue;
+
+        if (enemy.defeated === false) {
+            if (enemy.face === faces.right) {
+                enemy.face = faces.down;
+            } else {
+                enemy.face = faces.right;
+            }
+        }
+    }
+}
+
+
+function movePowerDot() {
+    if (powerDot.countDown === 0) {
+        if (powerDot.powerUp) {
+            context.fillStyle = "#ffffff";
+            
+            context.beginPath();
+            context.arc(powerDot.x, powerDot.y, powerDot.radio, 20, Math.PI * 2, true);
+            context.closePath();
+            context.fill();
+        } else {
+            powerDot.x = random(dimensions.width) + 30;
+            powerDot.y = random(dimensions.height);
+            powerDot.powerUp = true;
+        }
+    
+        checkPowerDot();
+    }
+}
+
+function checkPowerDot() {
+    if (playerOne.x <= (powerDot.x - powerDot.radio) && powerDot.x <= (playerOne.x + playerOne.width) 
+        && playerOne.y <= (powerDot.y - powerDot.radio) && powerDot.y <= (playerOne.y + playerOne.height)) {
+        powerDot.x = 0;
+        powerDot.y = 0;
+        powerDot.powerUp = false;
+        powerDot.countDown = 1000;
+        playerOne.speed = speds.fast;
+    }
+}
+
+function checkEnemyCollision(enemy) {
+    var deltaX = Math.abs(playerOne.x - enemy.x);
+    var deltaY = Math.abs(playerOne.y - enemy.y);
+    
+    if (deltaX <= enemy.width && deltaY <= enemy.height) {
+        var x = random(dimensions.width);
+        var y = random(dimensions.height);
+        
+        if (powerDot.countDown > 0) {
+            score++;
+            enemy.x = x - enemy.width;
+            enemy.y = y - enemy.height;
+            enemy.defeated = true;
+            enemy.face = faces.left;
+
+        } else {
+            enemyScore++;
+            playerOne.x = x - playerOne.width;
+            playerOne.y = y - playerOne.height;
+        }
+
+        checkIfYouWon();
+    }
+}
+
+
+function checkIfYouWon() {
+    var defeated = enemies.filter(e => e.defeated);
+    
+    if (defeated.length === enemies.length) {
+        alert("You Win");
+        resetGame();
+    } else if (enemyScore === maxTries) {
+        alert("You Lose");
+        resetGame();
+    }
+
+}
+
+function checkTimeoutPowerDot() {
+    if (powerDot.countDown > 0) {
+        powerDot.countDown--;
+    } else {
+        playerOne.speed = speds.normal;
     }
 }
 
 document.addEventListener("keydown", function(event){
     keyclick[event.keyCode] = true
-    movePacman(keyclick)
+    movePlayerOne(keyclick)
 }, false);
 
 document.addEventListener("keyup", function(event){
